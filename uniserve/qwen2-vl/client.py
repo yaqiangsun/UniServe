938f58eabe0fcf22668828e7b87209b957630428
yaqiang.sun <sunyaking@163.com>
2024-12-11 05:15:12 +0000

    add local image as qwen2-vl input in client

--- uniserve/qwen2-vl/client.py
+++ uniserve/qwen2-vl/client.py
@@ -11,19 +11,35 @@
 import argparse
 from openai import OpenAI
 from termcolor import colored
+import base64
 
 MODELS_LIST = {
     "Qwen2-VL-7B": "Qwen/Qwen2-VL-7B-Instruct",
     "Qwen2-VL-2B": "Qwen/Qwen2-VL-2B-Instruct",
 }
 
+
 DEFAULT_MODEL = MODELS_LIST["Qwen2-VL-7B"]
 
 
 client = OpenAI(base_url="http://0.0.0.0:8085/v1",api_key="EMPTY")
 
+def image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:  # open image with binary mode
+        image_data = image_file.read()  # read image data
+        base64_encoded_data = base64.b64encode(image_data)  # Base64 encode
+        base64_string = base64_encoded_data.decode('utf-8')  # convert to utf-8 string
+    return base64_string
+
 def chat_request(prompt="图片中有什么？",image_url = f"file://XXXXXXXXXX.jpg"):
-    image_url = "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg"
+
+    # image url as image input
+    # image_url = "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg"
+
+    # read image file and encode to base64 as input
+    image_path = "./demo.jpeg"
+    image_data = image_to_base64(image_path)
+    image_url = f"data:image/jpeg;base64,{image_data}"
    
     
     stream = client.chat.completions.create(
