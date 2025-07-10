df2b66e1926c58dad7c7e0f9916365bd05c7fce0
sunyaqiang <sunyaking@163.com>
2025-07-10 09:36:33 +0000

    add qwen2.5-vl

---
+++ uniserve/qwen2_5-vl/client.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2024 yaqiang.sun.
+# This source code is licensed under the license found in the LICENSE file
+# in the root directory of this source tree.
+#########################################################################
+# Author: yaqiangsun
+# Created Time: 2024/12/10 17:25:05
+########################################################################
+
+
+import argparse
+from openai import OpenAI
+from termcolor import colored
+import base64
+
+MODELS_LIST = {
+    "Qwen2-VL-7B": "Qwen/Qwen2-VL-7B-Instruct",
+    "Qwen2-VL-2B": "Qwen/Qwen2-VL-2B-Instruct",
+}
+
+
+DEFAULT_MODEL = MODELS_LIST["Qwen2-VL-7B"]
+
+
+client = OpenAI(base_url="http://0.0.0.0:9000/v1",api_key="EMPTY")
+
+def image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:  # open image with binary mode
+        image_data = image_file.read()  # read image data
+        base64_encoded_data = base64.b64encode(image_data)  # Base64 encode
+        base64_string = base64_encoded_data.decode('utf-8')  # convert to utf-8 string
+    return base64_string
+
+def chat_request(prompt="图片中有什么？",image_url = f"file://XXXXXXXXXX.jpg"):
+
+    # image url as image input
+    # image_url = "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg"
+
+    # read image file and encode to base64 as input
+    image_path = "./demo.jpeg"
+    image_data = image_to_base64(image_path)
+    image_url = f"data:image/jpeg;base64,{image_data}"
+   
+    
+    stream = client.chat.completions.create(
+        model=DEFAULT_MODEL,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": prompt},
+                    {
+                        "type": "image",
+                        "image_url": image_url,
+                        "resized_height": 280,
+                        "resized_width": 420
+                    },
+                ],
+            }
+        ],
+        stream=True,
+        max_tokens=1024,
+    )
+    print(colored("Runing...", "yellow"))
+    for chunk in stream:
+        print(colored(chunk.choices[0].delta.content or "", "green"), end="")
+
+if __name__ == "__main__":
+    prompt="Detect all objects in the image and return their locations in the form of coordinates. The format of output should be like {“bbox”: [x1, y1, x2, y2], “label”: the name of this object in Chinese}"
+    chat_request(prompt=prompt)
+
+
---
+++ uniserve/qwen2_5-vl/run.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025 yaqiang.sun.
+# This source code is licensed under the license found in the LICENSE file
+# in the root directory of this source tree.
+#########################################################################
+# Author: yaqiangsun
+# Created Time: 2025/07/10 16:19:48
+########################################################################
+
+
+import litserve as ls
+from litserve.specs.openai import ChatCompletionRequest
+from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor, TextIteratorStreamer
+from PIL import Image
+import torch
+import io
+import base64
+from threading import Thread
+from qwen_vl_utils import process_vision_info
+import os
+
+
+
+# Add exception handling to thread
+class ThreadWithExceptionHandling(Thread):
+    def __init__(self, target, args=(), kwargs=None):
+        super().__init__(target=target, args=args, kwargs=kwargs)
+        self.target = target
+        self.args = args
+        self.kwargs = kwargs if kwargs is not None else {}
+    def run(self):
+        try:
+            self.target(*self.args, **self.kwargs)
+        except Exception as e:
+            print(f"Exception in thread: {e}")
+
+
+class QwenVLAPI(ls.LitAPI):
+    def setup(self, device):
+        # 多模态模型加载
+        model_id = "Qwen/Qwen2.5-VL-7B-Instruct"
+        self.model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+            model_id,
+            device_map="auto",
+            torch_dtype=torch.bfloat16,
+        ).eval()
+        self.processor = AutoProcessor.from_pretrained(model_id)
+        print(f"Model loaded on {device}")
+
+        self.streamer = TextIteratorStreamer(
+            self.processor.tokenizer,
+            skip_prompt=True,
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=False,
+        )
+
+    def decode_request(self, request: ChatCompletionRequest , context: dict):
+        # reload model
+        # if request.model != self.model_id:
+        #     self.setup(self.device, request.model)
+   
+        context["generation_args"] = {
+            "max_new_tokens": request.max_tokens if request.max_tokens else 2048,
+        }
+        messages = [
+            message.model_dump(exclude_none=True) for message in request.messages
+        ]
+
+        # add resized_height and resized_width to image
+        for message in (messages):
+            if message["role"] == "user":
+                for content_item in message["content"]:
+                    if content_item.get("type") == "image":
+                        content_item.update(
+                            {
+                                "resized_height": 280,
+                                "resized_width": 420
+                            }
+                        )
+
+   
+        text = self.processor.apply_chat_template(
+            messages, tokenize=False, add_generation_prompt=True
+        )
+        image_inputs, video_inputs = process_vision_info(messages)
+        inputs = self.processor(
+            text=[text],
+            images=image_inputs,
+            videos=video_inputs,
+            padding=True,
+            return_tensors="pt",
+        ).to(self.device)
+        return inputs
+
+    def predict(self, model_inputs, context: dict):
+        generation_kwargs = dict(
+            model_inputs,
+            streamer=self.streamer,
+            eos_token_id=self.processor.tokenizer.eos_token_id,
+            **context["generation_args"],
+        )
+        thread = ThreadWithExceptionHandling(target=self.model.generate, kwargs=generation_kwargs)
+        thread.start()
+        
+        for text in self.streamer:
+            yield text
+if __name__ == "__main__":
+    api = QwenVLAPI(
+                    spec=ls.OpenAISpec(),
+                    # max_batch_size=4,        # 批处理提升吞吐量 但是需要修改解析形式
+                    )
+    server = ls.LitServer(api,
+                          devices=1,          # 自动检测GPU "auto"
+                          workers_per_device=1,    # 每GPU进程数
+                          accelerator="cuda",
+                          )
+    server.run(port=9000, host="0.0.0.0")
